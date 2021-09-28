5770795286b4539c1686d65212d86409f9f10e97
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-09-28 15:04:34 +0700

    Add files via upload

---
+++ MASUKIN HURUF KECIL KE KAPITAL.py
@@ -0,0 +1,7 @@
+print ( "mengubah Huruf Kecil menjadi Kapital") 
+print( "silahkan masukan huruf yang ingin diubah") 
+a=input ()  
+#perintah input() untuk memasukkan string 
+print("menjadi =", a.upper()) 
+print("========================") 
+
